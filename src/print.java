aea2936ad0d6dc5c9606c40d92617cf4c92ed976
Bjarte Espedokken <bjarte.espedokken@gmail.com>
2016-03-07 20:27:08 +0100

    Lagt til print-klasse

---
+++ src/print.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+
+public class print extends connectDB {
+    
+    public void printTreningsøkt () {
+
+       try {
+           Statement stmt = conn.createStatement();
+           String query = "select * from økt";
+           
+           ResultSet rs = stmt.executeQuery(query);
+           rs.next();
+           
+           while(rs.next()){        	   
+    		   System.out.println(String.format("Dato: %s \nKommentar: %s \nVarighet: %s",rs.getString("dato"),rs.getString("kommentar"),rs.getString("varighet")));
+    		   System.out.println("");        		   
+           }
+       }
+            
+     	catch (Exception e) {
+            System.out.println("db error during select of loper = "+e);
+    	}
+
+    }
+    
+    public static void main(String[] args) {
+		print p = new print();
+		p.printTreningsøkt();
+	}
+  
+}
