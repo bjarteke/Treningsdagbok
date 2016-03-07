31c9c7c9ed48adaafca192b042f0f0e78a8116e5
Bjarte Espedokken <bjarte.espedokken@gmail.com>
2016-03-07 20:18:55 +0100

    Lagt til klasse som henter inn mySQL-koden.

--- src/connectDB.java
+++ src/connectDB.java
@@ -1,4 +1,23 @@
+import java.util.Properties;
+import java.sql.*;
 
-public class connectDB {
-
+public abstract class connectDB {
+    protected Connection conn;
+    
+    public connectDB () {
+    }
+   
+    public void connect() {
+    	try {
+    		Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Properties p = new Properties();
+            p.put("user", "bjarteke_bruker");
+            p.put("password", "pass1234");           
+            conn = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no/bjarteke_trening?autoReconnect=true&useSSL=false",p);
+        } catch (Exception e)
+    	{
+            throw new RuntimeException("Unable to connect", e);
+    	}
+    }
 }
+
