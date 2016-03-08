28d0e309e805026206e705f650ff2b8dc1d8807e
Bjarte Espedokken <bjarte.espedokken@gmail.com>
2016-03-08 09:13:46 +0100

    Added fnutts.

--- src/ØktRegistrerer.java
+++ src/ØktRegistrerer.java
@@ -1,32 +1,51 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+
+import com.mysql.jdbc.PreparedStatement;
 
 public class ØktRegistrerer extends connectDB{
 	
-	public static int øktId = 1;
-	
-	public void registerØkt(String tidspunkt, String varighet, String kommentar, String prestasjon, String aktivitet){
+	private void registerØkt(String tidspunkt, String varighet, String kommentar, String prestasjon, String aktivitet){
 		try {
-            Statement statement = conn.createStatement();
-            statement.executeUpdate("INSERT INTO økt " + "VALUES (" + Integer.toString(øktId) + "," + tidspunkt + "," + varighet + "," + kommentar + "," + prestasjon+ "," + aktivitet+")");
+			String s = "INSERT INTO økt(øktid,dato,varighet,kommentar,prestasjon,aktivitet) VALUES (4,'2016-07-03',0.5,'hdjkash',10, 'ute');";
+			java.sql.PreparedStatement pstmt = conn.prepareStatement(s);
+			pstmt.execute();
+			
+			/* pstmt.setString(1, "3");
+			 pstmt.setString(2, tidspunkt);
+			 pstmt.setString(3, varighet);
+			 pstmt.setString(4, kommentar);
+			 pstmt.setString(5, prestasjon);
+			 pstmt.setString(6, aktivitet);*/
+			 
         } catch (Exception e) {
-            System.out.println("db error during insert");
+            System.out.println("db error during insert"+e);
         }
 	}
 	
 	public void registerUteØkt(String tidspunkt, String varighet, String kommentar, String prestasjon, String aktivitet, String temperatur, String vær){
 		 try {
              Statement statement = conn.createStatement();
-             statement.executeUpdate("INSERT INTO utendørsaktivitet " + "VALUES (" + Integer.toString(øktId) + "," + "ute" + "," + temperatur + "," + vær + ")");
-             registerØkt(tidspunkt, varighet, kommentar, prestasjon, aktivitet);
-             øktId++;
+
+             registerØkt(tidspunkt, varighet, kommentar, prestasjon, aktivitet);             
+             statement.executeUpdate("INSERT INTO utendørsaktivitet(øktid, aktivitet,temperatur,vær) VALUES (4,ute,skey,bra)");
+
          } catch (Exception e) {
-             System.out.println("db error during insert");
+             System.out.println("db error during insert"+e);
          }
 		
 	}
 	
 	public void registrerInneØkt(String dato, String varighet, String kommentar, String prestasjon, String aktivitet, String luftkvalitet, String tilskuere) {
 		registerØkt(dato, varighet, kommentar, prestasjon,aktivitet);
+	}
+	
+	public static String getNextØktId(Statement stmt) throws SQLException{
+		String query = "select * from økt";
+		ResultSet rs = stmt.executeQuery(query);
+		rs.previous();		
+        return rs.getString("øktid");	
 	}
 	
 }
