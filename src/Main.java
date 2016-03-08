9d5d0cf147e440cab8936c0b96f76bae9611ca56
Bjarte Espedokken <bjarte.espedokken@gmail.com>
2016-03-08 09:39:13 +0100

    Main template

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {        
-        Print su = new Print ();
+        ØktRegistrerer su = new ØktRegistrerer();
+        
+        
         su.connect();
-        su.printTreningsøkt();
+  
+        
+        Scanner scanner = new Scanner(System.in);
+        //Alternativer til til å gjøre, går i loop.
+        //Legg til økt --> koble økt til øvelse(r)
+        	//Når du har lagt til resultat, sjekk om dette er oppnådd
+        //Se ti beste resultater fra gitt øvelse
+        //Se antall kilometer løpt
+        
+        
+        
+        
+        su.registerUteØkt("2016-03-07", "0.5", "Bra økt", "5", "akt", "24", "Skyet");
     }
 
 }
